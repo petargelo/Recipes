c0262f8da6d85988f8c61e6f8d7bd43b29182262
Petar Gelo <pgelo@gelo.com>
2021-06-04 12:41:15 +0200

    reating new component recipe-start that is shown when no recipes selected. It is routing child of recipes component. Adding router outlet to recipes component instead of ngIf and ng-template. Next step is to fix logic to pass the data so recipe detail can be seen on child page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,16 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch:'full'}, //it's possible to just add recipes as component instead of redirectTo
-    { path: 'recipes', component: RecipesComponent },
+    { path: 'recipes', component: RecipesComponent, 
+        children: [{path:'', component:RecipeStartComponent},
+                    {path: ':id', component: RecipeDetailComponent}]},
     { path: 'shopping-list', component: ShoppingListComponent }
 ]
-
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
