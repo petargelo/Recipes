ae715d31b14dd95864bbadd6baea2d632c419cc7
Petar Gelo <pgelo@gelo.com>
2022-01-13 13:13:34 +0100

    Added protection from errors if there is no ingredients within recipe (TypeError undefined). It is protected using rxjs map operator to transform data recieved from http request (i.e. from Firebase). After that I used user array map operator (different operator than rxjs map) to push an empty ingredient array if ingredients do not exist.

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
+import { map } from 'rxjs/operators'
+
+
+@Injectable({providedIn:'root'}) //@Injectable needed because we inject service (http) into service (data-storage)
+export class DataStorageService{
+    constructor(private http: HttpClient, private recipeService: RecipeService){}
+
+        storeRecipes(){
+            const recipes= this.recipeService.getRecipes(); //using recipeService method to store
+            this.http.put( // .put overwrites existing data with new data
+                'https://recipebookngapp-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+                recipes).subscribe(response=>{
+                    console.log(response);
+                }); //another way is to add return in front of put request and subscribe in header component. 
+                    //Useful if loading spinner should be shown because header component then knows if request is done or not. 
+
+        }
+
+        fetchRecipes(){
+            this.http.get<Recipe[]>(
+                'https://recipebookngapp-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+                .pipe(map(recipes=>{
+                    return recipes.map(recipe=>{
+                        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []}; //copy all recipes but change ingredients. If ingredients true (has 0 or more elements) write them, else write empty array. 
+                    }) 
+                })).subscribe(recipes=>{
+                    this.recipeService.setRecipes(recipes); 
+        })
+                                    //TypeScript doesn't understand recipes is really array. It sees it as body of http response.
+                                                            //I inform TS about type with adding type format -> <Recipe[]>
+  
+
+    
+}
+}
