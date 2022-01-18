0c3cb9d18daa996bb9b9b7f041fbf4ed33292da2
Petar Gelo <pgelo@gelo.com>
2022-01-18 17:40:53 +0100

    Creating auth service responsible for signing users up and managing tokens. Added method for posting to Firebase API. onSubmit method in Auth Component gives values of email and password to service for post method and subscribes to response from Firebase. Implemented if/else statement for checking in which mode user is (login/signup). Currently only signup is working as expected.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,22 +9,43 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {  }
 
   isLoginMode = true;
+  signupError= '';
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm)  {
-    console.log(form.value)
+    const email=form.value.email;
+    const password=form.value.password;
+
+    if (!form.valid) {
+      return;
+    }
+
+    if(this.isLoginMode){
+      console.log("U Login modu ste");}
+
+      else{
+      this.authService.signup(email, password).subscribe(resData=> 
+        {
+        console.log(resData);
+        },
+        error=>
+        {
+          this.signupError=error.error.error.message;
+          /* this.signupError=error.error.error.message; */
+        }
+      );
+    }
+
     form.reset();
+    }
+    
 }
 
-
-}
