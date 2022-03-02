a3e5b10a8604a37cd2fc7817fd64a4b626691e92
Petar Gelo <Petar.Gelo@intis.hr>
2022-03-02 13:10:02 +0100

    Minor cleaning of unused imports

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,6 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { LoadingSpinnerComponent } from "../shared/loading-spinner/loading-spinner.component";
 import { SharedModule } from "../shared/shared.module";
 import { AuthComponent } from "./auth.component";
 
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,10 +1,8 @@
 //this module holds everything that is related to recipes. This module is then imported to main Ngmodule - app.module
 
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { DropdownDirective } from "../shared/dropdown.directive";
 import { SharedModule } from "../shared/shared.module";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
@@ -21,17 +19,15 @@ import { RecipesComponent } from "./recipes.component";
         RecipeDetailComponent,
         RecipeItemComponent,
         RecipeStartComponent,
-        RecipeEditComponent,
-        
-        
+        RecipeEditComponent, 
     ],
 
     imports:[
         RouterModule,
         RecipesRoutingModule, //added so recipe routes can be browsed.
-       /*  CommonModule,   */ //added so ngIf, ngFor can be used. Added instead of browser module which does this work in app module.
+       //  CommonModule,       //added so ngIf, ngFor can be used. Added instead of browser module which does this work in app module.
         ReactiveFormsModule,
-        SharedModule
+        SharedModule    //RecipesModule gets CommonModule and Dropdown from SharedModule
     ],
 
     providers:[]
